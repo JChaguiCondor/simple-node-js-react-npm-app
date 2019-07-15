cff9b1e81badd5140d6f1dbe42bd697be386f5f1
JChagui <jchagui@condorlabs.io>
2019-07-15 08:44:59 -0500

    test error added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    return 
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,7 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    
   }
 }
 
